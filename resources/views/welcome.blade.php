c5a7222a13e349100efcabbbd7afc30c7d3b6698
Safeer E Hussain <safeer.husssain@gmail.com>
2026-05-03 16:06:46 +0500

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Arbaeen Ziyarat Packages from Pakistan » {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
